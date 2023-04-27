cf1bd01b374d7ec5cc4aeadb355a830608873f14
JORDAN HENRIQUE MOREIRA ZANONI <52969396+zanonijordan@users.noreply.github.com>
2023-04-27 18:55:22 +0100

    Create component TransactionContainer, TransactionTable, PriceHightLight

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -1,6 +1,10 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
-import * as Styled from './styles'
+import {
+  PriceHighLight,
+  TransactionContainer,
+  TransactionTable,
+} from './styles'
 
 export type TransactionsProps = {
   title?: string
@@ -8,9 +12,39 @@ export type TransactionsProps = {
 
 export const Transactions = ({ title }: TransactionsProps) => {
   return (
-    <Styled.Wrapper>
+    <div>
       <Header />
       <Summary />
-    </Styled.Wrapper>
+      <TransactionContainer>
+        <TransactionTable>
+          <tbody>
+            <tr>
+              <td width="50%">website development</td>
+              <td>
+                <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
+              </td>
+              <td>Sale</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td>
+                <PriceHighLight variant="outcome">- R$ 59,00</PriceHighLight>
+              </td>
+              <td>Food</td>
+              <td>10/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Apartment rent</td>
+              <td>
+                <PriceHighLight variant="outcome">- R$ 1.200,00</PriceHighLight>
+              </td>
+              <td>House</td>
+              <td>27/03/2022</td>
+            </tr>
+          </tbody>
+        </TransactionTable>
+      </TransactionContainer>
+    </div>
   )
 }
--- src/pages/transactions/styles.ts
+++ src/pages/transactions/styles.ts
@@ -1,5 +1,42 @@
 import styled, { css } from 'styled-components'
 
-export const Wrapper = styled.div`
-  ${({ theme }) => css``}
+export const TransactionContainer = styled.main`
+  ${({ theme }) => css`
+    width: 100%;
+    max-width: 1120px;
+    margin: 4rem auto 0;
+    padding: 0 1.5rem;
+  `}
+`
+export const TransactionTable = styled.table`
+  ${({ theme }) => css`
+    width: 100%;
+    border-collapse: separate;
+    border-spacing: 0 0.5rem;
+    margin-top: 1.5rem;
+
+    td {
+      padding: 1.25rem 2rem;
+      background-color: ${theme['gray-700']};
+
+      &:first-child {
+        border-top-left-radius: 6px;
+        border-bottom-left-radius: 6px;
+      }
+      &:last-child {
+        border-top-right-radius: 6px;
+        border-bottom-right-radius: 6px;
+      }
+    }
+  `}
+`
+
+interface PriceHighLightProps {
+  variant: 'income' | 'outcome'
+}
+export const PriceHighLight = styled.span<PriceHighLightProps>`
+  color: ${(props) =>
+    props.variant === 'income'
+      ? props.theme['green-300']
+      : props.theme['red-300']};
 `
